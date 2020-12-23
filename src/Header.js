df952d1c85d38d45745c83ab0f3b50be7eb99a85
wonhyoek <epahsejtmxm@naver.com>
2020-12-23 20:04:36 +0900

    Header 2

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import Logo from "./robinhood.svg"
+import Logo from "./robinhood.svg";
+import "./Header.css";
 
 export default () => {
     return (
         <div className = "header__wrapper">
             <div className = "header__logo">
                 <img src = {Logo} width = {25}/>
+            </div>
+            <div className = "header__search">
+                <div className = "header__searchContainer">
+                    <input placeholder = "Search" type = "text"/>
+                </div>
+            </div>
+            <div className = "header__menuItems">
+                <a href="/">Free Stocks</a>
+                <a href="/">PortFolio</a>
+                <a href="/">Cash</a>
+                <a href="/">Messages</a>
+                <a href="/">Account</a>
             </div>
         </div>
     )
